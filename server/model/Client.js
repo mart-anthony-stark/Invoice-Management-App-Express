efb1841c3dd6a764979081867a20d8a46161eb52
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 20:23:50 +0800

    Added user_id relation

--- server/model/Client.js
+++ server/model/Client.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   name: { type: String, required: true },
   email: { type: String, required: true },
-  address: { type: String, required: true },
+  street_address: { type: String, required: true },
   city: { type: String, required: true },
   zip_code: { type: String, required: true },
   country: { type: String, required: true },
