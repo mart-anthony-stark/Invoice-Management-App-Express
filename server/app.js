cedbe08c7cc82e0ba2dec43caf74a23893566a65
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 19:43:41 +0800

    Connected mongodb

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 const port = process.env.PORT || 3000;
 require("dotenv").config({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/auth", require("./routes/auth.route"));
+
 app.listen(port, (e) => {
   if (e) return console.log(e);
   console.log(`Server started at port ${port}`);
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to database");
+    })
+    .catch((e) => console.log(e));
 });
-console.log(process.env.DB_URI);
