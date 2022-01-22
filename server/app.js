051f585c58f510aff39bce888273d0b5feafd42c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 15:34:02 +0800

    Added dotenv

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+require("dotenv").config({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -9,3 +10,4 @@ app.listen(port, (e) => {
   if (e) return console.log(e);
   console.log(`Server started at port ${port}`);
 });
+console.log(process.env.DB_URI);
