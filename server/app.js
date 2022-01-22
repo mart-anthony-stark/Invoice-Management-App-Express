3c456c773c82ce38b23ba0bbb6469b756e5dc8e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 15:24:58 +0800

    Fixed typo error

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(exress.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, (e) => {
