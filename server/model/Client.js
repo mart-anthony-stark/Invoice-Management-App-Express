b78edab43afd6fe49d666caebf3db957f14ae073
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 20:56:17 +0800

    Added timestamps

--- server/model/Client.js
+++ server/model/Client.js
@@ -8,6 +8,6 @@ const clientSchema = new mongoose.Schema({
   city: { type: String, required: true },
   zip_code: { type: String, required: true },
   country: { type: String, required: true },
-});
+},{timestamps: true});
 
 module.exports = mongoose.model("Client", clientSchema);
