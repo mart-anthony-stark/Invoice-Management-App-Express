007727af8c75d0235e2af2289cb74a1232207cd5
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 19:50:07 +0800

    Fixed token local variable

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -6,15 +6,16 @@ const createToken = (user) => {
 
 const isAuth = (req, res, next) => {
   if (!req.headers.token)
-    return res.status(401).json({ error: "Token is not defined" });
+    return res.status(401).json({ error: "Unauthorized" });
 
-  const token = req.headers.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "Token is not defined" });
+  const token = req.headers.token.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-    user.password = null;
-    console.log({ user });
-    req.user = user;
+  jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+    if (err) return res.status(401).json({ error: "Unauthorized" });
+
+    decodedToken.user.password = undefined;
+    req.user = decodedToken.user;
     next();
   });
 };
