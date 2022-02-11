85ac3bfa7dbb88d64eb1cbc4d61e98bd68bb2541
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-11 19:53:12 +0800

    add: sidebar component

---
+++ client/src/components/Sidebar.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<div class="sidebar" />
+
+<style>
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 300px;
+    background: var(--c-complementary);
+  }
+</style>
