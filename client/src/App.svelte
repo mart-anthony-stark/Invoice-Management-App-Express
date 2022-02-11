138c77ffca256b53de5a68e1ccf2c9485ad03e31
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-11 19:52:03 +0800

    add: sidebar

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,10 +2,19 @@
 </script>
 
 <main />
+<div class="sidebar" />
 
 <style>
   main {
     min-height: 100vh;
     background: var(--c-pri);
+  }
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 300px;
+    background: var(--c-complementary);
   }
 </style>
