ad62f6c1f5027803d5659d5f5bf8696b5c8a18f9
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-13 19:50:21 +0800

    add: auth route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,11 @@
 import Home from "./pages/Home.svelte";
+import Auth from "./pages/Auth.svelte";
 
 const routes = {
   "/": Home,
   "/dashboard": Home,
+  "/auth": Auth,
+  "/*": Home,
 };
 
 export { routes };
